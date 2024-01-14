64bc8400fdfec144f1660c33bb8228922f565eb7
Miguel Mendoza <miguel@zandome.com>
2024-01-14 11:26:20 -0400

    feat: add git hooks

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+const config = {
+  '**/*.ts?(x)': () => 'tsc -p tsconfig.build.json --noEmit',
+  '*.{js,jsx,ts,tsx}': ['npm run lint', 'jest --findRelatedTests --passWithNoTests'],
+  '*.{md,json}': 'prettier --write',
+};
+
+module.exports = config;
