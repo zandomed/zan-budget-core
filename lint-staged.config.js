59680c58f3a1609583f535e3a240abd9c104e529
Miguel Mendoza <miguel@zadndome.com>
2024-01-14 14:59:44 -0400

    style: modify lenght line in Prettier

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,9 @@
 const config = {
   '**/*.ts?(x)': () => 'tsc -p tsconfig.build.json --noEmit',
-  '*.{js,jsx,ts,tsx}': ['npm run lint', 'jest --findRelatedTests --passWithNoTests'],
+  '*.{js,jsx,ts,tsx}': [
+    'npm run lint',
+    'jest --findRelatedTests --passWithNoTests',
+  ],
   '*.{md,json}': 'prettier --write',
 };
 
